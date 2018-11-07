284773e5df120f0cb06ec338a8889c1780bede06
lanadrahrepus <sitdownrightnow2552@gmail.com>
2018-11-07 08:30:02 +0700

    add security config

---
+++ src/main/java/lana/configuration/Initializer/SecurityWebApplicationInit.java
@@ -0,0 +1,8 @@
+package lana.configuration.Initializer;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInit
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
